b5e7a228f233a34545e311482d83389bfdacfdab
giuliobarde <giuliobarde@gmail.comg@Giulios-MBP ai-tutor-backend %>
2024-07-29 21:14:34 +0200

    security commit 2

--- pantry/firebase.js
+++ pantry/firebase.js
@@ -1,20 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAoDpawm5uBBS70wYTr1dHiUkx4yiQct1U",
-  authDomain: "hspantryapp-c89b3.firebaseapp.com",
-  projectId: "hspantryapp-c89b3",
-  storageBucket: "hspantryapp-c89b3.appspot.com",
-  messagingSenderId: "143929449024",
-  appId: "1:143929449024:web:71a536059abb538894dda5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app)
-export {app, firestore}
+const firestore = getFirestore(app);
+export { app, firestore };
