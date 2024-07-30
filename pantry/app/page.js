264f7ab2a6ebcec5802aa2ac0c7ba61845136468
giuliobarde <giuliobarde@gmail.comg@Giulios-MBP ai-tutor-backend %>
2024-07-30 09:29:23 +0200

    modal creation commit

--- pantry/app/page.js
+++ pantry/app/page.js
@@ -1,12 +1,30 @@
 "use client";
 
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography, Button, Modal, Autocomplete, TextField } from "@mui/material";
 import { firestore } from "@/firebase";
 import { collection, getDocs, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
+const style = {             // 100% edit
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const foodSuggestions = []
+
 export default function Home() {
   const [pantry, setPantry] = useState([]); 
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     const updatePantry = async () => {
@@ -32,7 +50,32 @@ export default function Home() {
       justifyContent={'center'}
       flexDirection={'column'}
       alignItems={'center'}
+      gap={2}
     >
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack spacing={2} sx={{ width: 300 }}>
+            <Autocomplete
+              id="free-solo-demo"
+              freeSolo
+              options={foodSuggestions.map((option) => option.title)}
+              renderInput={(params) => <TextField {...params} label="Search Item" />}
+            />
+          </Stack>
+        </Box>
+      </Modal>
+      <Button 
+        variant="contained"
+        onClick={handleOpen}
+      >Add</Button>
       <Box
         border={'1px solid #333'}
       >
